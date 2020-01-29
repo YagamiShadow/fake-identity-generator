97c0e2149f129ec965e341c081e199fbe8a99973
smokingcuke <w0j8uhv5csio@opayq.net>
2020-01-29 11:05:57 +0100

    Start using Shellcheck, fix bugs. Add ideas + revamp data structs.

--- ident_gen.sh
+++ ident_gen.sh
@@ -1,58 +1,79 @@
 #!/bin/bash
+#shellcheck disable=SC2059
+#shellcheck disable=SC2034
 COUNTRY_LIST=countries.txt
 MALE_NAMES=male_names.txt
 FEMALE_NAMES=female_names.txt
-BLOOD_TYPE_LIST=blood_types.txt
-ETHNICITY_LIST=ethnicity.txt
-RELIGION_LIST=religions.txt
 PLACEHOLDER=null.txt
+
 FORMAT_STRING_TWO_COL="%-14s%s %s %s %s %s\n"
 count=1
 padding=14
-declare -A basicInformation
-declare -A financialInformation
-declare -A employmentInformation
-declare -A personalInformation
-declare -A onlineProfileInformation
-declare -A otherInformation
-numColumns=2
-numRows=5
-if [[ -e fields_and_filenames.txt && -s fields_and_filenames.txt ]] 
-then
-for ((i=1;i<=numRows;i++)) do
-	for ((j=1;j<=numColumns;j++)) do
-		basicInformation[$i,1]=$( awk 'BEGIN{FS=","} "FNR == $i {print $1; exit}"'  fields_and_filenames.txt )
-	done
-done
-else
-	exit
-fi
+declare -a basicInformation
+
+BLOODTYPE=(A+ A- B+ B- AB+ AB- O+ O-)
+SEX=(Male Female Non-Binary)
+SEXUAL_IDENTITY=(Heterosexual Bisexual Homosexual Transsexual)
+STATUS=(Single Married Engaged)
+ETHNICITY=(Arab Latino Caucasian Afro)
+RELIGION=(Islam Christianity Judaism Rastafari Buddhism Hinduism)
 
 printf '\f%s' "Enter number of identities you want to generate: "
-read numberOfIdentities
+read -r numberOfIdentities
 
-while [ $numberOfIdentities -ge 1 ]
+while [ "$numberOfIdentities" -ge 1 ]
 do
 	sex=$((1 + RANDOM % 2))
 	printf '%s\n' "========== ${count} =========="
 	printf "\n%s\n"		        "Basic Information"
 	if [ $sex -eq 1 ]
 	then
-		printf "$FORMAT_STRING_TWO_COL" "Name:" $( shuf -n 1 $MALE_NAMES )
+		printf "$FORMAT_STRING_TWO_COL" "Name:" "$( shuf -n 1 $MALE_NAMES )"
 		printf "$FORMAT_STRING_TWO_COL" "Sex:" "Male"
 	else
-		printf "$FORMAT_STRING_TWO_COL" "Name:" $( shuf -n 1 $FEMALE_NAMES )
+		printf "$FORMAT_STRING_TWO_COL" "Name:" "$( shuf -n 1 $FEMALE_NAMES )"
 		printf "$FORMAT_STRING_TWO_COL" "Sex:" "Female"
 	fi
 	printf "$FORMAT_STRING_TWO_COL" "Age: " $((15 + RANDOM % 80))
 	printf "$FORMAT_STRING_TWO_COL" "Height:"  $((110 + RANDOM % 210)) "cm"
 	printf "$FORMAT_STRING_TWO_COL" "Weight:"  $((50 + RANDOM % 150)) "kg"
-	for ((i=1;i<=numRows;i++)) do
-		printf "$FORMAT_STRING_TWO_COL" ${basicInformation[$i,1]}
-		#shuf -n 1 ${basicInformation[$i,2]}
-	done
-	count=$[ $count + 1 ]
-	numberOfIdentities=$[ $numberOfIdentities - 1]
+	# Phone
+	# Country code
+	# Mothers maiden name
+	# Geo coordinates ##.#####, -##.######
+	# SSN 
+
+	## Online identities
+	#Facebook
+	#	Email-address
+	#	username
+	#	password
+	#	Browser user-agent
+	#	
+	#Instagram
+	# ..
+	# ..
+	# ..
+	# Owned websites
+
+	# Employment
+	#	Company
+	# 	Occupation
+	# 	Salary
+
+	## Finance
+	# Credit card: #### #### #### ####
+	# Expires ##/##
+	# CVC2 ###
+
+	## Other
+	#	Favorite color
+	#	Vehicle
+	#	GUID
+	#	QR Code (generate)
+
+	count=$(( count + 1 ))
+	numberOfIdentities=$(( numberOfIdentities - 1 ))
 	echo
 done
 # Based on statistics of leaving the country (for example at a specific age, such as expats) the FROM should equal RESIDENCY in nn% of the time, e.g. 93% should have FROM=RESIDENCY
